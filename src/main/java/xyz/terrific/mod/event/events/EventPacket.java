46241dab3dcf2ce332ccf3a30c6d617ab5d9dc65
TerrificTable <omegagaming189@gmail.com>
2022-07-06 22:50:39 +0200

    finished EventPacket.java

--- src/main/java/xyz/terrific/mod/event/events/EventPacket.java
+++ src/main/java/xyz/terrific/mod/event/events/EventPacket.java
@@ -6,12 +6,50 @@ import xyz.terrific.mod.event.Event;
 // !!! NOT FINISHED !!!
 public class EventPacket extends Event<EventPacket> {
 
+    public Packet<?> packet;
+    public Mode mode;
+    public boolean cancelled;
+
     /**
      * Packet Event constructor
      * @param packet the packet
      * @param mode the mode
      */
-    public EventPacket(Packet<?> packet, Mode mode) {  }
+    public EventPacket(Packet<?> packet, Mode mode) {
+        this.packet = packet;
+        this.mode = mode;
+    }
+
+    /**
+     * get Packet Object
+     * @return packet Object
+     */
+    public Packet<?> getPacket() {
+        return packet;
+    }
+
+    /**
+     * get Mode
+     * @return mode
+     */
+    public Mode getMode() {
+        return mode;
+    }
+
+    /**
+     * get cancelled state
+     */
+    public void getCancelled() {
+        cancelled = true;
+    }
+
+    /**
+     * set cancelled state
+     * @param cancelled new Event cancelled state
+     */
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
 
     /**
      * Mode Enum
