bd2e89aaa7e91557acb83ba3e85be37443c13c01
TerrificTable <omegagaming189@gmail.com>
2022-07-08 00:30:38 +0200

    added ArrayList

--- src/main/java/xyz/terrific/mod/module/modules/render/hud/Hud.java
+++ src/main/java/xyz/terrific/mod/module/modules/render/hud/Hud.java
@@ -3,7 +3,8 @@ package xyz.terrific.mod.module.modules.render.hud;
 import xyz.terrific.mod.event.Event;
 import xyz.terrific.mod.event.events.EventRender2D;
 import xyz.terrific.mod.module.Module;
-import xyz.terrific.mod.module.modules.render.hud.elements.NameAndVersion;
+import xyz.terrific.mod.module.modules.render.hud.elements.Watermark;
+import xyz.terrific.mod.module.setting.settings.BooleanSetting;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,15 +14,19 @@ public class Hud extends Module {
     // hudElements List
     public List<HudElement> hudElements = new ArrayList<>();
 
+    public BooleanSetting Watermark = new BooleanSetting("Watermark", true);
+    public BooleanSetting ArrayList = new BooleanSetting("ArrayList", true);
 
     /**
      * Hud (this is a module) Constructor
      */
     public Hud() {
         super("Hud", 0, Category.RENDER);
+        this.addSettings(Watermark, ArrayList);
+        this.setToggled(true);
 
         // add hud element to hudElements List
-        hudElements.add(new NameAndVersion());
+        hudElements.add(new Watermark());
 
     }
 
@@ -35,7 +40,9 @@ public class Hud extends Module {
 
         // show every element in hudElements List
         for (HudElement elm : hudElements) { // iterate over elements in hudElements List
-            elm.show(eventRender2D); // show element
+            if (elm.showElement) {
+                elm.show(eventRender2D); // show element
+            }
         }
 
     }
