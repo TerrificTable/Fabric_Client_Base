35cc52c1a38ede02fce08c1aad921c1ad2c4cd1d
TerrificTable <omegagaming189@gmail.com>
2022-07-08 00:30:29 +0200

    not even started ArrayList class

---
+++ src/main/java/xyz/terrific/mod/module/modules/render/hud/elements/ArrayList.java
@@ -0,0 +1,22 @@
+package xyz.terrific.mod.module.modules.render.hud.elements;
+
+import xyz.terrific.mod.event.events.EventRender2D;
+import xyz.terrific.mod.module.modules.render.hud.HudElement;
+
+/**
+ * NOT FINISHED (NOT EVEN STARTED)
+ */
+public class ArrayList extends HudElement {
+    public ArrayList() {
+        super("ArrayList");
+        this.setPosition(new int[] { 10, this.getWidth() - 10 });
+        this.showElement = true;
+    }
+
+    @Override
+    public void show(EventRender2D event) {
+
+        // TODO
+
+    }
+}
